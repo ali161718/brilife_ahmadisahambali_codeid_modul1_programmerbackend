b9ea81fa63d641d3a98cb524445f948ad38eade2
Ahmad Isa Hambali <izam.hambali@gmail.com>
2020-07-04 08:27:14 +0700

    initiation project rest-service

---
+++ rest-service/src/main/resources/import.sql
@@ -0,0 +1,23 @@
+INSERT INTO `LIST_KONTRASEPSI` (`Id_Kontrasepsi`, `Nama_Kontrasepsi`) VALUES (1,'Pil');
+INSERT INTO `LIST_KONTRASEPSI` (`Id_Kontrasepsi`, `Nama_Kontrasepsi`) VALUES (2,'Kondom');
+INSERT INTO `LIST_KONTRASEPSI` (`Id_Kontrasepsi`, `Nama_Kontrasepsi`) VALUES (3,'IUD');
+
+INSERT INTO `LIST_PROPINSI` (`Id_Propinsi`, `Nama_Propinsi`) VALUES (1,'Aceh');
+INSERT INTO `LIST_PROPINSI` (`Id_Propinsi`, `Nama_Propinsi`) VALUES (2,'Sumatera Utara');
+INSERT INTO `LIST_PROPINSI` (`Id_Propinsi`, `Nama_Propinsi`) VALUES (3,'Sumatera Barat');
+INSERT INTO `LIST_PROPINSI` (`Id_Propinsi`, `Nama_Propinsi`) VALUES (4,'Riau');
+INSERT INTO `LIST_PROPINSI` (`Id_Propinsi`, `Nama_Propinsi`) VALUES (5,'Kepulauan Riau');
+INSERT INTO `LIST_PROPINSI` (`Id_Propinsi`, `Nama_Propinsi`) VALUES (6,'Jambi');
+INSERT INTO `LIST_PROPINSI` (`Id_Propinsi`, `Nama_Propinsi`) VALUES (7,'Bangka Belitung');
+INSERT INTO `LIST_PROPINSI` (`Id_Propinsi`, `Nama_Propinsi`) VALUES (8,'Sumatera Selatan');
+INSERT INTO `LIST_PROPINSI` (`Id_Propinsi`, `Nama_Propinsi`) VALUES (9,'Lampung');
+
+INSERT INTO `LIST_PEMAKAI_KONTRASEPSI` (`Id_List`, `Id_Propinsi`, `Id_Kontrasepsi`, `Jumlah_Pemakai`) VALUES (1,1,1,50);
+INSERT INTO `LIST_PEMAKAI_KONTRASEPSI` (`Id_List`, `Id_Propinsi`, `Id_Kontrasepsi`, `Jumlah_Pemakai`) VALUES (2,1,2,66);
+INSERT INTO `LIST_PEMAKAI_KONTRASEPSI` (`Id_List`, `Id_Propinsi`, `Id_Kontrasepsi`, `Jumlah_Pemakai`) VALUES (3,1,3,25);
+INSERT INTO `LIST_PEMAKAI_KONTRASEPSI` (`Id_List`, `Id_Propinsi`, `Id_Kontrasepsi`, `Jumlah_Pemakai`) VALUES (4,2,1,100);
+INSERT INTO `LIST_PEMAKAI_KONTRASEPSI` (`Id_List`, `Id_Propinsi`, `Id_Kontrasepsi`, `Jumlah_Pemakai`) VALUES (5,2,2,75);
+INSERT INTO `LIST_PEMAKAI_KONTRASEPSI` (`Id_List`, `Id_Propinsi`, `Id_Kontrasepsi`, `Jumlah_Pemakai`) VALUES (6,2,3,50);
+INSERT INTO `LIST_PEMAKAI_KONTRASEPSI` (`Id_List`, `Id_Propinsi`, `Id_Kontrasepsi`, `Jumlah_Pemakai`) VALUES (7,1,2,40);
+INSERT INTO `LIST_PEMAKAI_KONTRASEPSI` (`Id_List`, `Id_Propinsi`, `Id_Kontrasepsi`, `Jumlah_Pemakai`) VALUES (8,2,2,65);
+INSERT INTO `LIST_PEMAKAI_KONTRASEPSI` (`Id_List`, `Id_Propinsi`, `Id_Kontrasepsi`, `Jumlah_Pemakai`) VALUES (9,3,1,90);
